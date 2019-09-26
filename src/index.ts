265565c9c62b955a361219323c891fc57256dc38
Kristijan Ristovski <kristijan.mkd@gmail.com>
2019-09-26 12:02:30 +0200

    add contextWithRequest

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import { Photon } from "@generated/photon";
 import { GraphQLServer } from "graphql-yoga";
+import { contextWithRequest } from "graphql-user";
 import { permissions } from "./shield/permissions";
 import { schema } from "./schema";
 const photon = new Photon();
@@ -8,12 +9,9 @@ const photon = new Photon();
 const server = new GraphQLServer({
   schema,
   middlewares: [permissions],
-  context: request => {
-    return {
-      ...request,
-      photon
-    };
-  }
+  context: contextWithRequest({
+    photon
+  })
 });
 
 server.start(() => console.log(`🚀 Server ready at: http://localhost:4000 `));
